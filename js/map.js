90cd0d6cbe416700d6e3215ad026858698fa1ef9
ErikoX <ponotone@gmail.com>
2022-08-27 19:23:40 +0200

    Made Utils and Help window, added view toggleables and added canvas resizing

--- js/map.js
+++ js/map.js
@@ -1,9 +1,11 @@
 const container = document.getElementById("gridd")
+const moveable = document.getElementById("grid-move")
 const imggrid = document.getElementById("imggrid")
 var selectedimg = null;
 var movex = 0;
 var movey = 0;
 var mult = 3;
+var celltouch = false
 var placeMode = false;
 var deleteMode = false
 document.body.addEventListener("keydown", function(e) {
@@ -27,14 +29,14 @@ document.body.addEventListener("keyup", function (e) {
 })
 
 document.body.addEventListener("mousedown", function(e) {
-  switch (e.button){
+  switch (e.button && celltouch){
     case 0: if(!deleteMode){placeMode=true}; break; 
     case 2: if(!placeMode){deleteMode=true}; break; 
   }
-  console.log(placeMode,deleteMode,e.button)
 })
 
 document.body.addEventListener("mouseup", function(e) {
+  celltouch = false
   switch (e.button){
     case 0: placeMode=false; break; 
     case 2: deleteMode=false; break; 
@@ -48,23 +50,34 @@ function resetMapHtml() {
 
 function makeRows(rows, cols) {
   for (let i = 0; i < rows; i++) {
-    let div = document.createElement("div")
-    div.classList.add("d-flex")
-    for (let a = 0; a < cols; a++) {
-      let cell = document.createElement("div")
-      cell.innerHTML = " "
-      cell.classList.add("cell")
-      cell.onmouseenter = function(){touchedCell(this);}
-      cell.onmousedown = function(){clickedCell(this);}
-      cell.oncontextmenu = function(e){e.preventDefault();deleteCell(this)}
-      div.appendChild(cell)
-      
-    }
+    let div = makeRow(cols)
 
     container.appendChild(div)
     
   }
 };
+
+function makeRow(cols) {
+  let div = document.createElement("div")
+  div.classList.add("d-flex")
+  for (let a = 0; a < cols; a++) {
+    let cell = makeCell()
+    div.appendChild(cell)
+    
+  }
+  return div
+}
+
+function makeCell() {
+  let cell = document.createElement("div")
+  cell.innerHTML = " "
+  cell.id = " "
+  cell.classList.add("cell")
+  cell.onmouseenter = function(){touchedCell(this);}
+  cell.onmousedown = function(){clickedCell(this);}
+  cell.oncontextmenu = function(e){e.preventDefault();deleteCell(this)}
+  return cell
+}
 
 resetMapHtml()
 
@@ -136,6 +149,7 @@ function selectImage(img) {
 }
 
 function touchedCell(cell) {
+  celltouch = true
   if (placeMode) {
     clickedCell(cell)
   }
@@ -189,13 +203,62 @@ function clipboardFromText(id) {
   
 }
 
+function map_addCol() {
+  let rows = container.children
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    if (row.children.length < 100) {
+      row.appendChild(makeCell())
+    }
+    
+    
+  }
+}
+function map_remCol() {
+  let rows = container.children
+  for (let i = 0; i < rows.length; i++) {
+    const cols = rows[i].children;
+    if (cols.length > 3) {
+      cols[cols.length - 1].remove()
+    }
+    
+    
+  }
+  
+}
 
+function map_addRow() {
+  let cellcount = container.children[0].children.length
+  let row = makeRow(cellcount)
+  container.appendChild(row)
+}
+function map_remRow() {
+  let rows = container.children
+  if (rows.length > 3)rows[rows.length-1].remove()
+  
+}
+
+
+
+
+
+
+
+
+
+
+const addSide = document.getElementById("plusminusside")
+const addBottom = document.getElementById("plusminusbottom")
 
 
 function loop() {
+  if (movex != 0 || movey != 0) {
+    moveable.style.left = String(parseInt(moveable.style.left)+movex)+"px"
+    moveable.style.top = String(parseInt(moveable.style.top)+movey)+"px"
+  }
+  addSide.style.left = container.offsetWidth/2+30 + "px"
+  addBottom.style.top = container.offsetHeight/2+30 + "px"
   
-  container.style.left = String(parseInt(container.style.left)+movex)+"px"
-  container.style.top = String(parseInt(container.style.top)+movey)+"px"
   requestAnimationFrame(loop)
 }
 
